574e2f18a05246f585e3d04039c9623a07111fcf
heedaelee <adguy72@gmail.com>
2023-06-05 16:45:30 +0900

    회원가입 import 구문

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,13 +1,6 @@
-import React, {useCallback, useRef, useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Pressable,
-  Alert,
-} from 'react-native';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React from 'react';
+import {Text, View} from 'react-native';
 import {RootStackParamList} from '../../App';
 
 type SignUpScreenProps = NativeStackScreenProps<RootStackParamList, 'SignUp'>;
