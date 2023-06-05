804f2a25cafa9a0f2d0d50201785115460ac4641
heedaelee <adguy72@gmail.com>
2023-06-05 17:09:31 +0900

    회원가입 코드 추가 & dismisskeyboard구성

---
+++ src/components/DismissKeyBoardView.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleProp,
+  ViewStyle,
+} from 'react-native/types';
+import from 'react-native-ke'
+
+const DismissKeyboardView = () => (
+  <TouchableWithoutFeedback></TouchableWithoutFeedback>
+);
+
+export default DismissKeyboardView;
