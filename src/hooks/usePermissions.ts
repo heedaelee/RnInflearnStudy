649b8749032d40411dd2659c8b7deeda505079e7
heedaelee <adguy72@gmail.com>
2023-06-18 13:45:21 +0900

    RN 권한까지 저장

---
+++ src/hooks/usePermissions.ts
@@ -0,0 +1,8 @@
+import {useEffect} from 'react';
+import {Alert, Linking, Platform} from 'react-native';
+import {check, PERMISSIONS, request, RESULTS} from 'react-native-permissions';
+
+
+function usePermissions() {
+  
+}
